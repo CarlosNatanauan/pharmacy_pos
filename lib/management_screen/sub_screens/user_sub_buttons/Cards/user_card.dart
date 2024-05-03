9048fa4c9aa86f4687c81bdc4e07b8d4ea64512f
Carlos Natanauan <carlosbenedictn@gmail.com>
2024-05-03 13:13:19 +0800

    semi ayos na user man

---
+++ lib/management_screen/sub_screens/user_sub_buttons/Cards/user_card.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+
+class UserCard extends StatelessWidget {
+  final String firstName;
+  final String middleName;
+  final String lastName;
+  final String username;
+  final String password;
+  final String imageUrl;
+
+  const UserCard({
+    Key? key,
+    required this.firstName,
+    required this.middleName,
+    required this.lastName,
+    required this.username,
+    required this.password,
+    required this.imageUrl,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8),
+        color: containerColor,
+      ),
+      padding: EdgeInsets.all(15),
+      margin: EdgeInsets.only(bottom: 20),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          // Image
+          Padding(
+            padding: const EdgeInsets.only(right: 10),
+            child: Image.asset(
+              imageUrl,
+              width: screenWidth * 0.07, // Adjust based on screen size
+              height: screenWidth * 0.07, // Adjust based on screen size
+              fit: BoxFit.cover,
+            ),
+          ),
+          // User Info
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Name
+                Text(
+                  '$firstName $middleName $lastName',
+                  style: TextStyle(
+                    color: white,
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: screenWidth * 0.005), // Adjust based on screen size
+                // Credentials
+                Text(
+                  'Username: $username\nPassword: $password',
+                  style: TextStyle(
+                    color: white.withOpacity(0.7),
+                    fontSize: 13,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(width: 10),
+          // Edit and Delete Buttons
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  // Add delete functionality here
+                },
+                child: Text(
+                  'Delete',
+                  style: TextStyle(color: white),
+                ),
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: white,
+                  backgroundColor: Colors.red.withOpacity(0.7), // Use a different color for delete button
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: () {
+                  // Add edit functionality here
+                },
+                child: Text(
+                  'Edit',
+                  style: TextStyle(color: white),
+                ),
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: white,
+                  backgroundColor: primaryColor,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+const primaryColor = Color(0xFF685BFF);
+const canvasColor = Color(0xFF2E2E48);
+const scaffoldBackgroundColor = Color(0xFF464667);
+const white = Colors.white;
+const containerColor = Color(0xFF353550);
+final actionColor = const Color(0xFF5F5FA7).withOpacity(0.6);
+final divider = Divider(color: white.withOpacity(0.3), height: 1);
--- lib/management_screen/sub_screens/user_sub_buttons/user_management.dart
+++ lib/management_screen/sub_screens/user_sub_buttons/user_management.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'Cards/user_card.dart';
+
 void main() {
   runApp(MaterialApp(
     home: UserManagement(),
@@ -9,98 +11,117 @@ void main() {
 class UserManagement extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final screenWidth = MediaQuery.of(context).size.width;
-
     return Scaffold(
       backgroundColor: scaffoldBackgroundColor,
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(20.0),
-          child: GridView.builder(
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 4, // Number of items per row
-              crossAxisSpacing: 15.0, // Spacing between items horizontally
-              mainAxisSpacing: 15.0, // Spacing between items vertically
-            ),
-            itemCount: 5, // Placeholder for number of users
-            itemBuilder: (context, index) {
-              // Placeholder user data
-              String firstName = "John";
-              String middleName = "Doe";
-              String lastName = "Smith";
-              String username = "johnsmith";
-              String password = "********";
-              String imageUrl = "assets/images/img.png"; // Placeholder image URL
-
-              return SizedBox(
+          child: Row(
+            children: [
+              // Container for Cashier role
+              Expanded(
                 child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    color: canvasColor,
-                  ),
                   padding: EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    color: canvasColor,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.2),
+                        blurRadius: 5,
+                        offset: Offset(0, 3),
+                      ),
+                    ],
+                  ),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      // Image
-                      Center(
-                        child: Padding(
-                          padding: const EdgeInsets.only(bottom: 5.0),
-                          child: Image.asset(
-                            imageUrl,
-                            width: screenWidth * 0.07, // Adjust based on screen size
-                            height: screenWidth * 0.07, // Adjust based on screen size
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-                      // Full Name
+                    children: [
                       Text(
-                        '$firstName $middleName $lastName',
+                        'Cashier(s)',
                         style: TextStyle(
                           color: white,
-                          fontSize: 14,
+                          fontSize: 20,
                           fontWeight: FontWeight.bold,
                         ),
                       ),
-                      SizedBox(height: screenWidth * 0.005), // Adjust based on screen size
-                      // User Credentials
-                      Text(
-                        'Username: $username\nPassword: $password',
-                        style: TextStyle(
-                          color: white.withOpacity(0.7),
-                          fontSize: 13,
-                        ),
-                      ),
-                      SizedBox(height: screenWidth * 0.008), // Adjust based on screen size
-                      // Edit Button
-                      Align(
-                        alignment: Alignment.centerRight,
-                        child: ElevatedButton(
-                          onPressed: () {
-                            // Add edit functionality here
-                          },
-                          child: Text(
-                            'Edit',
-                            style: TextStyle(color: white,
-                            ),
-
-                          ),
-                          style: ElevatedButton.styleFrom(
-
-                            foregroundColor: white,
-                            backgroundColor: primaryColor,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(8),
-                            ),
+                      SizedBox(height: 20),
+                      Expanded(
+                        child: SingleChildScrollView(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: List.generate(5, (index) {
+                              return Padding(
+                                padding: const EdgeInsets.only(bottom: 10),
+                                child: UserCard(
+                                  firstName: "Diwata",
+                                  middleName: "Pares",
+                                  lastName: "Overload",
+                                  username: "diwata-123",
+                                  password: "********",
+                                  imageUrl: "assets/images/diwata.png",
+                                ),
+                              );
+                            }),
                           ),
                         ),
                       ),
                     ],
                   ),
                 ),
-              );
-            },
+              ),
+              SizedBox(width: 20),
+              // Container for Store Manager role
+              Expanded(
+                child: Container(
+                  padding: EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    color: canvasColor,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.2),
+                        blurRadius: 5,
+                        offset: Offset(0, 3),
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Store Manager(s)',
+                        style: TextStyle(
+                          color: white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Expanded(
+                        child: SingleChildScrollView(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: List.generate(2, (index) {
+                              return Padding(
+                                padding: const EdgeInsets.only(bottom: 10),
+                                child: UserCard(
+                                  firstName: "Doc",
+                                  middleName: "Willie",
+                                  lastName: "Ong",
+                                  username: "docwillie-123",
+                                  password: "********",
+                                  imageUrl: "assets/images/doc.png",
+                                ),
+                              );
+                            }),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
@@ -111,7 +132,8 @@ class UserManagement extends StatelessWidget {
 const primaryColor = Color(0xFF685BFF);
 const canvasColor = Color(0xFF2E2E48);
 const scaffoldBackgroundColor = Color(0xFF464667);
-const accentCanvasColor = Color(0xFF3E3E61);
+const containerColor = Color(0xFF353550);
 const white = Colors.white;
+
 final actionColor = const Color(0xFF5F5FA7).withOpacity(0.6);
 final divider = Divider(color: white.withOpacity(0.3), height: 1);
