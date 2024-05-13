14a0e4a49e7a31f3d9ce388886d31ae4fb89d465
Carlos Natanauan <carlosbenedictn@gmail.com>
2024-05-13 09:44:48 +0800

    redefined ui

--- lib/management_screen/sub_screens/medicine_sub_buttons/medicine_list.dart
+++ lib/management_screen/sub_screens/medicine_sub_buttons/medicine_list.dart
@@ -12,183 +12,186 @@ class MedicineList extends StatelessWidget {
     return Scaffold(
       backgroundColor: scaffoldBackgroundColor,
       body: Padding(
-        padding: const EdgeInsets.all(20.0),
+        padding: const EdgeInsets.symmetric(horizontal: 20),
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             // Left Container
-            SingleChildScrollView( // Making the container scrollable
-              child: Container(
-                width: MediaQuery.of(context).size.width * 0.28,
-                padding: EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                  color: canvasColor,
-                  borderRadius: BorderRadius.circular(20),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.2),
-                      blurRadius: 5,
-                      offset: Offset(0, 3),
-                    ),
-                  ],
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text(
-                      'Add Medicine',
-                      style: TextStyle(
-                        color: white,
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
+            Padding(
+              padding: EdgeInsets.only(bottom: 20.0),
+              child: SingleChildScrollView( // Making the container scrollable
+                child: Container(
+                  width: MediaQuery.of(context).size.width * 0.28,
+                  padding: EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    color: canvasColor,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.2),
+                        blurRadius: 5,
+                        offset: Offset(0, 3),
                       ),
-                    ),
-                    SizedBox(height: 20),
-
-                    TextFormField(
-                      style: TextStyle(color: white),
-                      decoration: InputDecoration(
-                        filled: true,
-                        fillColor: containerColor,
-                        hintText: 'SKU', // Adding hint text
-                        hintStyle: TextStyle(color: white.withOpacity(0.5)), // Hint text color
-                        border: OutlineInputBorder(),
+                    ],
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        'Add Medicine',
+                        style: TextStyle(
+                          color: white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                    ),
-                    SizedBox(height: 20),
+                      SizedBox(height: 20),
 
-                    TextFormField(
-                      style: TextStyle(color: white),
-                      decoration: InputDecoration(
-                        filled: true,
-                        fillColor: containerColor,
-                        hintText: 'Barcode number', // Adding hint text
-                        hintStyle: TextStyle(color: white.withOpacity(0.5)), // Hint text color
-                        border: OutlineInputBorder(),
+                      TextFormField(
+                        style: TextStyle(color: white),
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: containerColor,
+                          hintText: 'SKU', // Adding hint text
+                          hintStyle: TextStyle(color: white.withOpacity(0.5)), // Hint text color
+                          border: OutlineInputBorder(),
+                        ),
                       ),
-                    ),
-                    SizedBox(height: 20),
+                      SizedBox(height: 20),
 
-                    // Dropdown for Category
-                    DropdownButtonFormField<String>(
-                      decoration: InputDecoration(
-                        filled: true,
-                        fillColor: containerColor,
-                        border: OutlineInputBorder(),
-                        hintText: 'Category',
-                        hintStyle: TextStyle(color: white.withOpacity(0.5)),
+                      TextFormField(
+                        style: TextStyle(color: white),
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: containerColor,
+                          hintText: 'Barcode number', // Adding hint text
+                          hintStyle: TextStyle(color: white.withOpacity(0.5)), // Hint text color
+                          border: OutlineInputBorder(),
+                        ),
                       ),
-                      items: <String>['Category A', 'Category B', 'Category C', 'Category D']
-                          .map((String value) {
-                        return DropdownMenuItem<String>(
-                          value: value,
-                          child: Text(value, style: TextStyle(color: white)),
-                        );
-                      }).toList(),
-                      onChanged: (String? value) {},
-                    ),
-                    SizedBox(height: 20),
+                      SizedBox(height: 20),
 
-                    // Dropdown for Type
-                    DropdownButtonFormField<String>(
-                      decoration: InputDecoration(
-                        filled: true,
-                        fillColor: containerColor,
-                        border: OutlineInputBorder(),
-                        hintText: 'Type',
-                        hintStyle: TextStyle(color: white.withOpacity(0.5)),
+                      // Dropdown for Category
+                      DropdownButtonFormField<String>(
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: containerColor,
+                          border: OutlineInputBorder(),
+                          hintText: 'Category',
+                          hintStyle: TextStyle(color: white.withOpacity(0.5)),
+                        ),
+                        items: <String>['Category A', 'Category B', 'Category C', 'Category D']
+                            .map((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value, style: TextStyle(color: white)),
+                          );
+                        }).toList(),
+                        onChanged: (String? value) {},
                       ),
-                      items: <String>['Type 1', 'Type 2', 'Type 3', 'Type 4']
-                          .map((String value) {
-                        return DropdownMenuItem<String>(
-                          value: value,
-                          child: Text(value, style: TextStyle(color: white)),
-                        );
-                      }).toList(),
-                      onChanged: (String? value) {},
-                    ),
-                    SizedBox(height: 20),
+                      SizedBox(height: 20),
 
-                    TextFormField(
-                      style: TextStyle(color: white),
-                      decoration: InputDecoration(
-                        filled: true,
-                        fillColor: containerColor,
-                        hintText: 'Product name', // Adding hint text
-                        hintStyle: TextStyle(color: white.withOpacity(0.5)), // Hint text color
-                        border: OutlineInputBorder(),
+                      // Dropdown for Type
+                      DropdownButtonFormField<String>(
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: containerColor,
+                          border: OutlineInputBorder(),
+                          hintText: 'Type',
+                          hintStyle: TextStyle(color: white.withOpacity(0.5)),
+                        ),
+                        items: <String>['Type 1', 'Type 2', 'Type 3', 'Type 4']
+                            .map((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value, style: TextStyle(color: white)),
+                          );
+                        }).toList(),
+                        onChanged: (String? value) {},
                       ),
-                    ),
-                    SizedBox(height: 20),
+                      SizedBox(height: 20),
 
-                    TextFormField(
-                      style: TextStyle(color: white),
-                      decoration: InputDecoration(
-                        filled: true,
-                        fillColor: containerColor,
-                        hintText: 'Measurement', // Adding hint text
-                        hintStyle: TextStyle(color: white.withOpacity(0.5)), // Hint text color
-                        border: OutlineInputBorder(),
+                      TextFormField(
+                        style: TextStyle(color: white),
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: containerColor,
+                          hintText: 'Product name', // Adding hint text
+                          hintStyle: TextStyle(color: white.withOpacity(0.5)), // Hint text color
+                          border: OutlineInputBorder(),
+                        ),
                       ),
-                    ),
-                    SizedBox(height: 20),
+                      SizedBox(height: 20),
 
-                    TextFormField(
-                      style: TextStyle(color: white),
-                      decoration: InputDecoration(
-                        filled: true,
-                        fillColor: containerColor,
-                        hintText: 'Description', // Adding hint text
-                        hintStyle: TextStyle(color: white.withOpacity(0.5)), // Hint text color
-                        border: OutlineInputBorder(),
+                      TextFormField(
+                        style: TextStyle(color: white),
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: containerColor,
+                          hintText: 'Measurement', // Adding hint text
+                          hintStyle: TextStyle(color: white.withOpacity(0.5)), // Hint text color
+                          border: OutlineInputBorder(),
+                        ),
                       ),
-                    ),
-                    SizedBox(height: 20),
+                      SizedBox(height: 20),
 
-                    TextFormField(
-                      style: TextStyle(color: white),
-                      decoration: InputDecoration(
-                        filled: true,
-                        fillColor: containerColor,
-                        hintText: 'Product price', // Adding hint text
-                        hintStyle: TextStyle(color: white.withOpacity(0.5)), // Hint text color
-                        border: OutlineInputBorder(),
+                      TextFormField(
+                        style: TextStyle(color: white),
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: containerColor,
+                          hintText: 'Description', // Adding hint text
+                          hintStyle: TextStyle(color: white.withOpacity(0.5)), // Hint text color
+                          border: OutlineInputBorder(),
+                        ),
                       ),
-                    ),
-                    SizedBox(height: 20),
+                      SizedBox(height: 20),
 
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        ElevatedButton(
-                          onPressed: () {},
-                          child: Text('Save'),
-                          style: ElevatedButton.styleFrom(
-                            padding: EdgeInsets.symmetric(vertical: 16, horizontal: 24),
-                            foregroundColor: white,
-                            backgroundColor: Colors.green,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(8),
+                      TextFormField(
+                        style: TextStyle(color: white),
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: containerColor,
+                          hintText: 'Product price', // Adding hint text
+                          hintStyle: TextStyle(color: white.withOpacity(0.5)), // Hint text color
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      SizedBox(height: 20),
+
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {},
+                            child: Text('Save'),
+                            style: ElevatedButton.styleFrom(
+                              padding: EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+                              foregroundColor: white,
+                              backgroundColor: Colors.green,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8),
+                              ),
                             ),
                           ),
-                        ),
-                        SizedBox(width: 10),
-                        ElevatedButton(
-                          onPressed: () {},
-                          child: Text('Clear'),
-                          style: ElevatedButton.styleFrom(
-                            padding: EdgeInsets.symmetric(vertical: 16, horizontal: 24),
-                            foregroundColor: white,
-                            backgroundColor: Colors.red.withOpacity(0.7),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(8),
+                          SizedBox(width: 10),
+                          ElevatedButton(
+                            onPressed: () {},
+                            child: Text('Clear'),
+                            style: ElevatedButton.styleFrom(
+                              padding: EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+                              foregroundColor: white,
+                              backgroundColor: Colors.red.withOpacity(0.7),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8),
+                              ),
                             ),
                           ),
-                        ),
-                      ],
-                    ),
-                  ],
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
@@ -197,6 +200,7 @@ class MedicineList extends StatelessWidget {
             Expanded(
               child: Container(
                 padding: EdgeInsets.all(20),
+                margin: EdgeInsets.only(bottom: 20),
                 decoration: BoxDecoration(
                   color: canvasColor,
                   borderRadius: BorderRadius.circular(20),
